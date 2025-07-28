7191f9f0117b5d360a474fa0065d97a2ae67d650
Kiran Anand <liteninkiran@hotmail.com>
2025-07-28 16:30:57 +0100

    Move component

---
+++ src/components/SpaceMission.js
@@ -0,0 +1,88 @@
+// React
+import { useMemo, useState } from 'react';
+
+// AG Grid
+import { AgGridReact } from 'ag-grid-react';
+
+// Data
+import { data } from '../data';
+
+const SpaceMission = () => {
+    const [rowData, setRowData] = useState(data);
+
+    const defaultColDefMemoFn = () => ({
+        flex: 1,
+        filter: true,
+        floatingFilter: true,
+        editable: true,
+    });
+
+    const rowClassRuleMemoFn = () => ({
+        'red-row': (p) => p.data.company === 'SpaceX',
+    });
+
+    const rowSelectionMemoFn = () => ({
+        mode: 'multiRow',
+        checkboxes: 'mission',
+        headerCheckbox: true,
+        enableSelectionWithoutKeys: true,
+        enableClickSelection: true,
+    });
+
+    // Column Definitions: Defines the columns to be displayed.
+    const [colDefs, setColDefs] = useState([
+        {
+            field: 'mission',
+            valueGetter: (p) => p.data.mission + ' ' + p.data.price,
+            // checkboxSelection: true,
+        },
+        {
+            field: 'company',
+        },
+        {
+            field: 'location',
+        },
+        {
+            field: 'date',
+        },
+        {
+            field: 'time',
+        },
+        {
+            field: 'rocket',
+        },
+        {
+            field: 'price',
+            valueFormatter: (p) => '£' + p.value.toLocaleString(),
+            cellClassRules: {
+                'green-cell': (p) => p.value > 20000000,
+            },
+        },
+        {
+            field: 'successful',
+        },
+    ]);
+
+    const defaultColDef = useMemo(defaultColDefMemoFn, []);
+    const rowClassRules = useMemo(rowClassRuleMemoFn, []);
+    const rowSelection = useMemo(rowSelectionMemoFn, []);
+
+    return (
+        <div style={{ height: '100vh' }}>
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={colDefs}
+                theme='legacy'
+                className='ag-theme-quartz'
+                defaultColDef={defaultColDef}
+                rowSelection={rowSelection}
+                pagination={true}
+                paginationPageSize={15}
+                paginationPageSizeSelector={[10, 15, 20, 25, 50, 100]}
+                rowClassRules={rowClassRules}
+            />
+        </div>
+    );
+};
+
+export default SpaceMission;
