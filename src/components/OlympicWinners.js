3c6c412e9db9e06a0b7c9c68994a1b03da0fb7a1
Kiran Anand <liteninkiran@hotmail.com>
2025-07-29 22:31:56 +0100

    7-1: Reset

--- src/components/OlympicWinners.js
+++ src/components/OlympicWinners.js
@@ -10,8 +10,8 @@ const url = 'https://www.ag-grid.com/example-assets/olympic-winners.json';
 
 const colDefs = [
     { field: 'athlete' },
-    { field: 'year', filter: MyFilter, filterParams: { title: 'Year Filter' } },
-    { field: 'age', filter: MyFilter, filterParams: { title: 'Age Filter' } },
+    { field: 'year', filter: MyFilter },
+    { field: 'age' },
     { field: 'country' },
     { field: 'date' },
     { field: 'sport' },
